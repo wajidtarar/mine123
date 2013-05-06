82344340ee1ae24cea9226e1da11a7cdab4d090f
wajidtarar <wajid.tarar@gmail.com>
2013-05-06 23:31:22 +0500

    orig

---
+++ untitled folder/ScrollViews/PagedScrollViewController.h
@@ -0,0 +1,21 @@
+//
+//  PagedScrollViewController.h
+//  ScrollViews
+//
+//  Created by Matt Galloway on 01/03/2012.
+//  Copyright (c) 2012 Swipe Stack Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+
+@interface PagedScrollViewController : UIViewController <UIScrollViewDelegate>
+{
+    AppDelegate *appDelegate;
+
+}
+@property (nonatomic, strong) IBOutlet UIScrollView *scrollView;
+@property (nonatomic, strong) IBOutlet UIPageControl *pageControl;
+
+@end
